0c64d9a45140732096c98c11b7e7082a7fc9350d
peptide-s <xuzhiyuan2017@gmail.com>
2021-04-11 21:49:08 +0100

    add loi gauss gamma

--- codes/main.cpp
+++ codes/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <stdio.h>
 //#include <vector>
 //#include <string>
 #include "LoiExpon.hpp"
+#include "LoiGauss.hpp"
+#include "LoiGamma.hpp"
 using namespace std;
 
 int main(){
@@ -9,5 +12,21 @@ int main(){
     cout << Expon.density(1) << endl;
     cout << Expon.fctRepar(1) << endl;
     cout << Expon.VaR(0.05) << endl;
+    cout << endl;
+
+
+    LoiGauss Gau(0,1);
+    cout << Gau.density(1) << endl;
+    cout << Gau.fctRepar(1) << endl;
+    //cout << Gauss.VaR(0.05) << endl;
+    cout << endl;
+
+    LoiGamma Gam(1,1);
+    cout << Gam.density(2) << endl;
+    cout << Gam.fctRepar(2) << endl;
+    //cout << Gauss.VaR(0.05) << endl;
+    cout << endl;
+
+
 
 }
